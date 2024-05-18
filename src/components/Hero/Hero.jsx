fbc41e11afff597be7d978287fc6eda33f61c2d4
Trent <trent.joyner.gmail.com>
2024-05-18 18:13:00 -0400

    forgot to save a file before submitting

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
+// import React from 'react';
 
-import trio_sticker from '../../assets/HeroPic.jpg';
+// import trio_sticker from '../../assets/HeroPic.jpg';
 
-const Hero = () => {
-    return (
-        <div className= 'hero'>
-            <img src={trio_sticker} alt="" style={{width: '30%', height: 'auto'}}/>
-            <h1><span>Trent Joyner</span> Web Developer</h1>
-            <p>I am a web developer from North Carolina that is learning something new every day.</p>
-            <div className="hero-action">
-                <div className="hero-connect">Connect with me</div>
-                <div className="hero-resume">My Resume</div>
-            </div>
-        </div>
-    )
-}
+// const Hero = () => {
+//     return (
+//         <div className= 'hero'>
+//             <img src={trio_sticker} alt="" style={{width: '30%', height: 'auto'}}/>
+//             <h1><span>Trent Joyner</span> Web Developer</h1>
+//             <p>I am a web developer from North Carolina that is learning something new every day.</p>
+//             <div className="hero-action">
+//                 <div className="hero-connect">Connect with me</div>
+//                 <div className="hero-resume">My Resume</div>
+//             </div>
+//         </div>
+//     )
+// }
 
-export default Hero;
+// export default Hero;
